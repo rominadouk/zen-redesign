2e8e62e190f8ac6c27db8d3773f3aa411f560953
rominadouk <douk.romina@gmail.com>
2023-09-06 22:22:07 -0700

    initial commit for zen redesign

---
+++ src/modules/home-page/home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Zen</h1>
+            <ul id="mobile-nav-ul">
+                <li>Journal</li>
+                <li>Goals</li>
+                <li>Habits</li>
+                <li>User Icon</li>
+            </ul>
+            <ul style="display:none" id="desktop-nav">
+            </ul>
+        </div>
+     );
+}
+ 
+export default Home;
