f1bb22609d2f2809cb0906ffaf357e30db0d5a28
rominadouk <douk.romina@gmail.com>
2023-09-07 13:14:29 -0700

    added svg icons, and added layout for mobile nav

--- src/modules/home-page/home.tsx
+++ src/modules/home-page/home.tsx
@@ -1,15 +1,7 @@
 const Home = () => {
     return ( 
         <div>
-            <h1>Zen</h1>
-            <ul id="mobile-nav-ul">
-                <li>Journal</li>
-                <li>Goals</li>
-                <li>Habits</li>
-                <li>User Icon</li>
-            </ul>
-            <ul style="display:none" id="desktop-nav">
-            </ul>
+
         </div>
      );
 }
