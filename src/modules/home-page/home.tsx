e31b16668747d3744970714b7f4751f73640e4b9
rominadouk <douk.romina@gmail.com>
2023-10-03 22:16:45 -0700

    adjusted most styling on home page

--- src/modules/home-page/home.tsx
+++ src/modules/home-page/home.tsx
@@ -6,21 +6,21 @@ const Home = () => {
         <div>
             {/* Main Top Image + Image Description*/}
             <div id='home-main-img' className='w-screen h-60 lg:h-96'></div>
-            <div className='mx-4 mt-3 md:flex md:flex-row  md:justify-center'>
-                <h1 className='font-bold text-4xl md:w-80 md:place-self-center'>Transform Your Life, One Self-Care Session at a Time</h1>
+            <div className='mx-4 mt-9 md:flex md:flex-row md:justify-center lg:mt-16 lg:mx-20'>
+                <h1 className='font-bold text-4xl  mb-4 md:w-80 md:place-self-center lg:place-self-start lg:text-5xl lg:w-96 lg:mr-4'>Transform Your Life, One Self-Care Session at a Time</h1>
                 <div>
-                <p className='mt-1 md:w-96 md:ml-10'>Don't wait any longer to revitalize your life. Zen is your partner in self-care, designed to fit seamlessly into your daily routine. Start your journey towards a happier, healthier you today. Download Zen and embark on a transformational path of self-discovery and well-being.</p>
-                <div className='mb-7 mt-3 flex flex-row justify-center'>
-                    <button className='bg-sea-green-blue hover:bg-blue-500 text-off-white font-semibold py-2 px-4 hover:border-transparent rounded mr-2'>Try Zen for Free</button>
-                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-sea-green-blue hover:border-transparent rounded'>Download</button>
-                </div>
-
+                    <p className='mt-1 md:w-96 md:ml-10 lg:mt-0 lg:w-96'>Don't wait any longer to revitalize your life. Zen is your partner in self-care, designed to fit seamlessly into your daily routine. Start your journey towards a happier, healthier you today. Download Zen and embark on a transformational path of self-discovery and well-being.</p>
+                    {/* BUTTONS TOP */}
+                    <div className=' flex flex-row mt-2 md:ml-10'>
+                        <button className='bg-sea-green-blue hover:bg-blue-500 text-off-white font-semibold py-2 px-4 hover:border-transparent rounded mr-2'>Try Zen for Free</button>
+                        <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-sea-green-blue hover:border-transparent rounded'>Download</button>
+                    </div>
                 </div>
 
             </div>
             {/* Stand Out Section */}
-            <h2 className='font-bold text-2xl mt-7 ml-7 lg:mb-10 lg:text-3xl lg:mt-20 lg:ml-20'>Why Zen Stands Out</h2>
-            <section id='stand-out-container' className='mx-7 mb-5 md:flex md:flex-row lg:mx-20' >
+            <h2 className='font-bold text-2xl mt-7 ml-7  mb-3 lg:mb-10 lg:text-3xl lg:mt-20 lg:ml-20'>Why Zen Stands Out</h2>
+            <section id='stand-out-container' className='mx-7 mb-16 md:flex md:flex-row lg:mx-20' >
                 <div className='flex flex-col'>
                     <div id='stand-out-img1' className='stand-out-imgs'></div>
                     <p className='font-bold text-lg mt-2'>Daily Wellness Reminders</p>
@@ -42,28 +42,28 @@ const Home = () => {
             <section id='well-being-container' className='flex flex-col justify-center md:flex-row mx-3 my-4 md:mt-32 lg:mx-14'>
 
                 {/* Medium View:Left Column, Well-Being Start */}
-                <div className="md:flex md:flex-col md:w-3/4 md:mt-5 mr-16">
+                <div className=" mx-7 md:flex md:flex-col md:w-3/4 md:mt-5 mr-16">
                     <div className="">
                         <h2 className='font-bold text-2xl mb-2'>Experience a New Level of Well-Being</h2>
-                        <div id='well-being-img-mobile' className='well-being-img md:hidden'></div>
+                        <div id='well-being-img-mobile' className='well-being-img  place-self md:hidden'></div>
                         <p className="mt-2">Life can get overwhelming, and self-care often takes a backseat. But now, with our revolutionary self-care app, you can finally prioritize your mental, emotional, and physical health like never before.</p>
                     </div>
                     {/* Icon Columns */}
                     <div className='my-6 md:flex md:flex-row '>
                         <div className='mb-3 md:mr-1 md:px-2 md:mb-0'>
-                            <TargetIcon />
+                            <TargetIcon className="mb-2" />
                             <p className='font-bold'>Personalized Self-Care Plans</p>
                             <p>Say goodbye to one-size-fits-all approaches. Our app crafts personalized self-care plans tailored to your unique needs and goals. Whether you want to reduce stress, improve sleep, or boost your confidence, we've got you covered.</p>
                         </div>
                         <div className='md:px-2'>
-                            <SupportIcon />
+                            <SupportIcon className="mb-2"/>
                             <p className='font-bold'>Community and Support</p>
                             <p>Connect with like-minded individuals on our platform, sharing experiences, tips, and inspiration. Join a community that understands the importance of self-care and self-discovery.</p>
                         </div>
                     </div>
                 </div>
                 {/* Medium View:Left Column, Well-Being End */}
-                <div id='well-being-img-desktop' className='well-being-img' ></div>
+                <div id='well-being-img-desktop' className='well-being-img hidden lg:block' ></div>
             </section>
             {/* Revitalized Section */}
             <section className="lg:flex lg:flex-row lg:justify-between lg:mx-14 lg:my-20">    
