f3afbcd8316fab3cd1318c0091da7ea4e50fa210
rominadouk <douk.romina@gmail.com>
2023-10-03 21:02:54 -0700

    adjusted layout for medium size home page

--- src/modules/Navbar.tsx
+++ src/modules/Navbar.tsx
@@ -3,18 +3,22 @@ import {ReactComponent as JournalIcon} from '../assets/journal-icon.svg'
 import {ReactComponent as GoalIcon} from '../assets/flag-icon.svg'
 import {ReactComponent as HabitIcon} from '../assets/habits-icon.svg'
 import {ReactComponent as SelfCareIcon} from '../assets/selfcare-icon.svg'
-import { Link } from 'react-router-dom'
+import { ReactComponent as HomeIcon } from '../assets/house-icon.svg';
+import { Link, useNavigate } from 'react-router-dom'
 
 
 
 const Navbar = () => {
     return ( 
         <div>
-            <nav id="mobile-navbar" className="flex flex-row justify-center bg-light-brown lg:justify-start md:hidden flex-wrap">
+            <nav id='mobile-navbar' className='flex flex-row justify-center bg-light-brown lg:justify-start md:hidden flex-wrap'>
                     {/* Mobile Navbar */}
                         <h1 className='flex flex-column justify-center text-3xl font-brand basis-full my-3 text-4xl'>zen</h1>
                     {/* <h1 className='flex flex-column justify-center text-3xl font-header'>zen</h1> */}
                     <ul id="mobile-nav-ul" className='flex flex-row space-x-12 pb-2'>
+                        <Link to='/'>
+                            <li className='h-8 w-8'><HomeIcon/></li>
+                        </Link>
                         <li className='h-8 w-8'><SelfCareIcon/></li>
                         <li className='h-8 w-8'> <JournalIcon /></li>
                         <Link to='/goals'>
@@ -24,12 +28,12 @@ const Navbar = () => {
                     </ul>
             </nav>
 
-            <nav id="desktop-navbar" className="hidden md:flex bg-light-brown flex-row h-16 justify-between">
+            <nav id='desktop-navbar' className="hidden md:flex bg-light-brown flex-row h-16 justify-between">
                 {/* Desktop Navbar */}
                 <h1 className='basis-1/6 font-brand text-5xl place-self-center text-center'>zen</h1>
-                <ul id="desktop-nav-ul" className="flex text-xl space-x-12 place-self-center text-center">
+                <ul id='desktop-nav-ul' className='flex text-xl space-x-12 place-self-center text-center'>
                     <Link to='/'>
-                    <li className='hover:text-sea-green-blue'>Home</li>
+                        <li className='hover:text-sea-green-blue'>Home</li>
                     </Link>
                     <li className='hover:text-sea-green-blue'>Self-Care</li>
                     <li className='hover:text-sea-green-blue'>Journal</li>
@@ -44,7 +48,7 @@ const Navbar = () => {
                     <p className='text-l hover:text-sea-green-blue hover:underline'>Logout</p>
                     </div>             
                     {/* Insert avatar here */}
-                    <div className=' bg-off-white rounded-full h-12 w-12 place-self-center ml-4'> </div>
+                    <div className='bg-off-white rounded-full h-12 w-12 place-self-center ml-4'> </div>
                 </section>
             </nav>
 
--- src/modules/home-page/home.tsx
+++ src/modules/home-page/home.tsx
@@ -1,37 +1,76 @@
-import { ReactComponent as RelaxedImg } from "./images/relaxed.svg";
+import { ReactComponent as TargetIcon } from "./images/target-icon.svg";
+import { ReactComponent as SupportIcon} from "./images/support-icon.svg"
 
 const Home = () => {
     return ( 
         <div>
+            {/* Main Top Image + Image Description*/}
             <div id='home-main-img' className='w-screen h-52'></div>
-            <div className="mx-4 mt-3">
-                <h1 className="font-bold text-3xl">Transform Your Life, One Self-Care Session at a Time</h1>
-                <p className="mt-1">Don't wait any longer to revitalize your life. Zen is your partner in self-care, designed to fit seamlessly into your daily routine. Start your journey towards a happier, healthier you today. Download Zen and embark on a transformational path of self-discovery and well-being.</p>
+            <div className='mx-4 mt-3 md:flex md:flex-row  md:justify-center'>
+                <h1 className='font-bold text-4xl md:w-80'>Transform Your Life, One Self-Care Session at a Time</h1>
+                <p className='mt-1 md:w-96 md:ml-10'>Don't wait any longer to revitalize your life. Zen is your partner in self-care, designed to fit seamlessly into your daily routine. Start your journey towards a happier, healthier you today. Download Zen and embark on a transformational path of self-discovery and well-being.</p>
             </div>
-            
-            <section id='stand-out-container' className="mx-7 my-5" >
-            <h2 className='font-bold text-2xl mb-2'>Why Zen Stands Out</h2>
+            {/* Stand Out Section */}
+            <h2 className='font-bold text-2xl mt-7 ml-7'>Why Zen Stands Out</h2>
+            <section id='stand-out-container' className='mx-7 mb-5 md:flex md:flex-row' >
                 <div className='flex flex-col'>
                     <div id='stand-out-img1' className='stand-out-imgs'></div>
-                    <p className='font-bold text-lg'>Daily Wellness Reminders</p>
+                    <p className='font-bold text-lg mt-2'>Daily Wellness Reminders</p>
                     <p>Life can be hectic, but our app ensures you never forget to care for yourself. Receive daily reminders to take a moment for self-care, whether it's a quick breathing exercise or a longer wellness ritual.</p>
                 </div>
-                <div className='flex flex-col my-4'>
+                <div className='flex flex-col my-6 md:my-0 md:mx-3'>
                     <div id='stand-out-img2' className='stand-out-imgs'></div>
-                    <p className='font-bold text-lg'>Progress Tracking</p>
+                    <p className='font-bold text-lg mt-2'>Progress Tracking</p>
                     <p>Watch your journey to better well-being unfold with our progress tracking feature. Celebrate your achievements, no matter how small, and stay motivated to make self-care a lifelong habit.</p>
                 </div>
                 <div className='flex flex-col'>
                     <div id='stand-out-img3' className='stand-out-imgs'></div>
-                    <p className='font-bold text-lg'>Guided Meditation and Mindfulness</p>
+                    <p className='font-bold text-lg mt-2'>Guided Meditation and Mindfulness</p>
                     <p>Achieve inner peace with our library of guided meditation sessions and mindfulness exercises. Our experienced self-care applications will lead you through practices designed to reduce anxiety and increase clarity.</p>
                 </div>
             </section>
-            <section id='well-being-container' className="flex flex-col justify-center mx-3">
-                <h2 className="font-bold text-2xl">Experience a New Level of Well-Being</h2>
-                <p className="mt-2">Life can get overwhelming, and self-care often takes a backseat. But now, with our revolutionary self-care app, you can finally prioritize your mental, emotional, and physical health like never before.</p>
 
+            {/* Experience Well-Being Section */}
+            <section id='well-being-container' className='flex flex-col justify-center md:flex-row mx-3 my-4 md:mt-32'>
+
+                {/* Medium View:Left Column, Well-Being Start */}
+                <div className="md:flex md:flex-col md:w-2/4 md:mt-5 mr-16">
+                    <div className="">
+                        <h2 className='font-bold text-2xl mb-2'>Experience a New Level of Well-Being</h2>
+                        <div id='well-being-img-mobile' className='well-being-img md:hidden'></div>
+                        <p className="mt-2">Life can get overwhelming, and self-care often takes a backseat. But now, with our revolutionary self-care app, you can finally prioritize your mental, emotional, and physical health like never before.</p>
+                    </div>
+                    {/* Icon Columns */}
+                    <div className='my-6 md:flex md:flex-row '>
+                        <div className='mb-3 md:mr-2 md:px-2 md:mb-0'>
+                            <TargetIcon />
+                            <p className='font-bold'>Personalized Self-Care Plans</p>
+                            <p>Say goodbye to one-size-fits-all approaches. Our app crafts personalized self-care plans tailored to your unique needs and goals. Whether you want to reduce stress, improve sleep, or boost your confidence, we've got you covered.</p>
+                        </div>
+                        <div className='md:px-2'>
+                            <SupportIcon />
+                            <p className='font-bold'>Community and Support</p>
+                            <p>Connect with like-minded individuals on our platform, sharing experiences, tips, and inspiration. Join a community that understands the importance of self-care and self-discovery.</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Medium View:Left Column, Well-Being End */}
+                <div id='well-being-img-desktop' className='well-being-img' ></div>
             </section>
+            {/* Revitalized Section */}
+            <section>    
+                <div className='mx-3'>
+                    <h3 className='font-bold text-4xl text-center'>Your revitalized life begins here.</h3>
+                    <p className='text-center'>Take the first step towards a renewed you.</p>
+                </div>
+                {/* Revitalized Buttons */}
+                <div className='mb-7 mt-3 flex flex-row justify-center'>
+                    <button className='bg-sea-green-blue hover:bg-blue-500 text-off-white font-semibold py-2 px-4 hover:border-transparent rounded mr-2'>Try Zen for Free</button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-sea-green-blue hover:border-transparent rounded'>Download</button>
+                </div>
+            </section>
+
+
 
 
         </div>
