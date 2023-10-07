5cac98c30894ed64266c71faedc1ba41bf245531
rominadouk <douk.romina@gmail.com>
2023-10-06 20:46:22 -0700

    set selfcare dropodown position relative to give stacking effect applied z index

--- src/modules/selfcare-page/self-care.tsx
+++ src/modules/selfcare-page/self-care.tsx
@@ -12,6 +12,7 @@ const SelfCare = () => {
 
     const navigate = useNavigate();
     const [selectedFeeling, setSelectedFeeling] = useState('Choose One');
+    const [selectedSelfCare, setSelectedSelfCare] = useState('Choose one')
 
     return ( 
         <div className='font-archivo'>
@@ -36,7 +37,56 @@ const SelfCare = () => {
                     {/* I Want To.. Dropdown */}
                     <div className='mx-5 px-8 pt-3 pb-8 mb-4 drop-shadow-xl bg-pure-white lg:h-80 md:mx-16 lg:ml-16 lg:mr-0'>
                         <p>I want to...</p>
-                        <SelfCareDropdown />
+                        {/* <SelfCareDropdown /> */}
+                        <div>
+                            <Menu as="div" className="relative inline-block text-left">
+                            <div className='relative flex flex-col w-60 lg:w-96'>
+                                <Menu.Button className="inline-flex  gap-x-1.5 rounded-md bg-white pl-3 lg:w-96 py-2 pl-3 text-sm font-semibold shadow-sm ring-1 ring-inset ring-black hover:bg-gray-50">{selectedSelfCare}
+                                    <ChevronDownIcon className="-mr-1 h-5 w-5" aria-hidden="true" />
+                                </Menu.Button>
+                            </div>
+
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="relative left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <div className='py-1 bg-pure-white'>
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <li className={( active ? 'px-4 py-2 bg-off-white list-none' :'px-4 py-2 text-sm list-none')} onClick={() => { setSelectedSelfCare('exercise')}}>exercise</li>
+                                        )}
+                                    </Menu.Item>
+                                    {/* Improve my Skin Care Item */}
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <li className={( active ? 'px-4 py-2 bg-off-white list-none' :'px-4 py-2 text-sm list-none')} onClick={() => { setSelectedSelfCare('skincare')}}>improve my skincare</li>
+                                        )}
+                                    </Menu.Item>
+                                    {/* Laugh Item */}
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <li className={( active ? 'px-4 py-2 bg-off-white list-none' :'px-4 py-2 text-sm list-none')} onClick={() => { setSelectedSelfCare('laugh')}}>laugh</li>
+                                    )}
+                                    </Menu.Item>
+                                    <form method="POST" action="#">
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <li className={( active ? 'px-4 py-2 bg-off-white list-none' :'px-4 py-2 text-sm list-none')} onClick={() => { setSelectedSelfCare('social')}}>be social</li>
+                                            )}
+                                        </Menu.Item>
+                                    </form>
+                                </div>
+                            </Menu.Items>
+                        </Transition>
+                        </Menu>
+
+                        </div>
                     </div>
                 </section>
 
@@ -81,7 +131,7 @@ const SelfCare = () => {
                                     leaveFrom="transform opacity-100 scale-100"
                                     leaveTo="transform opacity-0 scale-95"
                                 >
-                                    <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <Menu.Items className="absolute left-0  mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <div className="py-1 bg-pure-white">
                                             <Menu.Item>
                                             {({ active }) => (
