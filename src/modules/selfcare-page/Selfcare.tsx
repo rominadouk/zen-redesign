e001f0d729aead5e769ec4e4ff6274503f0c10e1
rominadouk <douk.romina@gmail.com>
2023-10-07 11:40:37 -0700

    adjusted anchor tags for broken links

--- src/modules/selfcare-page/Selfcare.tsx
+++ src/modules/selfcare-page/Selfcare.tsx
@@ -33,7 +33,7 @@ const SelfCare = () => {
                 {/* SECTION 1 - LEFT ON DESKTOP*/}
                 <section>
                     {/* DAILY CHALLENGE SECTION */}
-                    <div className="mx-5 px-8 pt-3 pb-8 mb-4 lg:mb-3 drop-shadow-xl bg-pure-white md:mx-16 lg:ml-16 lg:mr-0">
+                    <div className='mx-5 px-8 pt-3 pb-8 mb-4 lg:mb-3 drop-shadow-xl bg-pure-white md:mx-16 lg:ml-16 lg:mr-0'>
                         <p className='font-bold text-based mt-2 mb-3'>Daily Challenge</p>
                         <h2 className='text-2xl font-bold mb-1'>Have some alone time.</h2>
                         <p>For many, alone time is a great opportunity for self-reflection and self-discovery. It can also rejuvenate you by balancing your emotions and letting your body physically relax. Not only does this practice help improve your well-being, but it can also improve your relationships with others.</p>
@@ -43,10 +43,10 @@ const SelfCare = () => {
                         <p>I want to...</p>
                         {/* SelfCare Dropdown START */}
                         <div>
-                            <Menu as="div" className="relative inline-block text-left">
+                            <Menu as='div' className='relative inline-block text-left'>
                             <div className='relative flex flex-col w-60 lg:w-96'>
                                 <Menu.Button className="inline-flex  gap-x-1.5 rounded-md bg-white pl-3 lg:w-96 py-2 pl-3 text-sm font-semibold shadow-sm ring-1 ring-inset ring-black hover:bg-gray-50">{selectedSelfCare}
-                                    <ChevronDownIcon className="-mr-1 h-5 w-5" aria-hidden="true" />
+                                    <ChevronDownIcon className='-mr-1 h-5 w-5' aria-hidden='true' />
                                 </Menu.Button>
                             </div>
 
@@ -112,13 +112,17 @@ const SelfCare = () => {
                     <div className='mx-5 px-8 pt-3 pb-8 mb-4 lg:mb-3  drop-shadow-xl bg-pure-white md:mx-16 lg:ml-4'>
                         <p className='font-bold'>You achieved</p>
                         <h3 className='text-4xl font-bold text-sea-green-blue'>12 goals!</h3>
-                        <p>since you started! You're awesome! <a href='/goals' className='text-sea-green-blue underline'>Go to Goals.</a></p>
+                        <p>since you started! You're awesome!</p>
+                        <a href='/goals' className='text-sea-green-blue underline'><p>Go to Goals.</p></a>
+
                     </div>
                     {/* Habits Completed Div */}
                     <div className='mx-5 px-8 pt-3 pb-8 mb-4 lg:mb-3  drop-shadow-xl bg-pure-white md:mx-16 lg:ml-4'>
                         <p className='font-bold'>You completed</p>
                         <h3 className='text-4xl font-bold text-sea-green-blue'>2 of 12 habits</h3>
-                        <p>Consistency is key! <a href='/habits' className='text-sea-green-blue underline'>Go to Habits.</a></p>
+                        <p>Consistency is key!</p>
+                        <a href='/habits' className='text-sea-green-blue underline'><p>Go to Habits. </p></a>
+
                     </div>
                     {/* Today I feel Div */}
                     <div className='flex flex-col mx-5 px-8 pt-3 pb-8 mb-4  lg:mb-3 drop-shadow-xl bg-pure-white md:mx-16 lg:ml-4'>
