2bcd87b25f880344171484f1fc8961af289c41a9
rominadouk <douk.romina@gmail.com>
2023-10-12 17:36:31 -0700

    ternary for rendering journal information now workds, there is a slight delay need to be revisited when journals populated

--- src/modules/journal-page/journal.tsx
+++ src/modules/journal-page/journal.tsx
@@ -1,8 +1,29 @@
 import DailyTip from "../DailyTip";
 import { ReactComponent as JournalIcon } from '../../assets/journal-icon.svg'
 import { ReactComponent as AddIcon } from '../../assets/add-icon.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Journal = () => {
+    const [allJournals, setAllJournals] = useState([] as any)
+
+    const getJournals = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000/journals')
+            setAllJournals(response.data)
+        } catch(err) {
+            console.log(err)
+        }
+
+    }
+
+    useEffect(() => {
+        getJournals();
+    }, [])
+
+
+
+
     return ( 
         <div className='font-archivo'>
             <section id='goals-top' className='flex flex-row mt-12 mb-4 mx-5 md:mx-16 justify-between xl:mx-24'>
@@ -17,6 +38,26 @@ const Journal = () => {
                 </button>
             </section>
             <DailyTip />
+            <section id='journals-display' className='mx-5'>
+                {/* BEGIN TERNARY: If render Journals is true, render data, if not then display info for no existing journals*/}
+                {allJournals.length > 0 ? 
+                <div>
+                    {allJournals.map((journal:any)=> {
+                        return (
+                            <div key={journal._id}>
+                            <p>{journal.title}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                : 
+                <div className='flex flex-col text-center'>
+                    <p>Welcome!</p>
+                    <p>There's nothing here yet.</p>
+                    <p>Try adding your first journal entry!</p>
+                </div>}
+                {/* END TERNARY */}
+            </section>
         </div>
      );
 }
